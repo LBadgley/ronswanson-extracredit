15e4f63758ba92e9101e1fbbfd0423f2f2931c57
Laura Badgley <l.badgleyy@gmail.com>
2019-05-31 15:52:25 -0700

    created all facts container

---
+++ src/containers/AllFacts.js
@@ -0,0 +1,42 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Facts from '../components/Facts';
+import { connect } from 'react-redux';
+import { getFacts, getFactsLoading, getFactsError } from '../selectors/factsSelectors';
+import { fetchFacts } from '../actions/factsActions';
+
+class AllFacts extends PureComponent {
+  static propTypes = {
+    fetch: PropTypes.func.isRequired,
+    facts: PropTypes.array.isRequired,
+    loading: PropTypes.any.isRequired,
+    error: PropTypes.object
+  }
+
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+  render() {
+    const { facts, loading, error } = this.props;
+    if(loading) return <h1>Loading!</h1>;
+    return <Facts facts={facts} error={error} />;
+  }
+}
+
+const mapStateToProps = state => ({
+  facts: getFacts(state),
+  loading: getFactsLoading(state),
+  error: getFactsError(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch() {
+    dispatch(fetchFacts());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AllFacts);
