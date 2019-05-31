58e9cbcdadb7e0877831fb98ffd39ce0275d6f46
Laura Badgley <l.badgleyy@gmail.com>
2019-05-31 15:28:14 -0700

    facts reducer created

---
+++ src/reducers/factsReducer.js
@@ -0,0 +1,18 @@
+import { FETCH_FACTS, FETCH_FACTS_LOADING, FETCH_FACTS_ERROR } from '../actions/factsActions';
+
+const initialState = {
+  loading: false,
+  list: [],
+  error: null
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case FETCH_FACTS_LOADING:
+      return { ...state, loading: true };
+    case FETCH_FACTS:
+      return { ...state, error: null, loading: false, list: action.payload };
+    case FETCH_FACTS_ERROR:
+      return { ...state, loading: false, error: action.payload };
+  }
+}
