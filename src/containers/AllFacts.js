9a13e31f73020ae108a3e6d0a3e3a2472e346069
Laura Badgley <l.badgleyy@gmail.com>
2019-05-31 16:36:15 -0700

    HOC refactoring, next step: add count (fetch) to state

--- src/containers/AllFacts.js
+++ src/containers/AllFacts.js
@@ -1,28 +1,8 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
 import Facts from '../components/Facts';
 import { connect } from 'react-redux';
 import { getFacts, getFactsLoading, getFactsError } from '../selectors/factsSelectors';
 import { fetchFacts } from '../actions/factsActions';
-
-class AllFacts extends PureComponent {
-  static propTypes = {
-    fetch: PropTypes.func.isRequired,
-    facts: PropTypes.array.isRequired,
-    loading: PropTypes.any.isRequired,
-    error: PropTypes.object
-  }
-
-  componentDidMount() {
-    this.props.fetch(50);
-  }
-
-  render() {
-    const { facts, loading, error } = this.props;
-    if(loading) return <h1>Loading!</h1>;
-    return <Facts facts={facts} error={error} />;
-  }
-}
+import { withFetch } from './withFetch';
 
 const mapStateToProps = state => ({
   facts: getFacts(state),
@@ -39,4 +19,4 @@ const mapDispatchToProps = (dispatch) => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(AllFacts);
+)(withFetch(Facts));
